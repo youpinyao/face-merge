d3a10cdf2ffdb954f24244d67e5327ea85faf023
游品尧 <497400448@qq.com>
2018-06-14 14:43:30 +0800

    改端口

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ app.post('/merge', function (req, res) {
   })
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(3333, function () {
   var host = server.address().address;
   var port = server.address().port;
 

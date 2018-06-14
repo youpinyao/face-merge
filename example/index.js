de6284c95e7631adf57b1ff7129f82376965f626
游品尧 <497400448@qq.com>
2018-06-14 14:41:02 +0800

    demo

---
+++ example/index.js
@@ -0,0 +1,52 @@
+let file = null;
+
+$.get('./templates.json').then(res => {
+  $('#templates').html(res.map((item, i) => `
+    <label>
+      <input type="radio" ${i === 0 ? 'checked' : ''} name="template" value="${item.model_id}"/>
+      <div><img src="${item.src}" alt=""/></div>
+      <div>${item.name}</div>
+    </label>
+  `))
+});
+
+
+$('#file').bind('change', e => {
+  console.log('====================================');
+  console.log(e.target.files);
+  console.log('====================================');
+  const reader = new FileReader();
+
+  reader.onload = (e) => {
+    file = e.target.result;
+  }
+
+  reader.readAsDataURL(e.target.files[0]);
+});
+
+$('#merge').bind('click', e => {
+  if (!file) {
+    alert('请选择图片');
+    return;
+  }
+
+  $.post({
+    url: '/merge',
+    contentType: 'text/json',
+    data: JSON.stringify({
+      img_data: file,
+      model_id: $('input[name="template"]:checked').val(),
+    }),
+  }).then(res => {
+    if (res.ret == 0) {
+      $('#box').html(`
+        <img src="${res.img_url}" style="width: 300px; height: auto;" />
+      `);
+    } else {
+      $('#box').html(JSON.stringify(res));
+    }
+  }, res => {
+    $('#box').html(JSON.stringify(res));
+  })
+});
+
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const getSign = require('./sign');
 const byteLength = require('byte-length').byteLength;
 const express = require('express');
 const request = require('request-promise');
+const bodyParser = require('body-parser');
 const app = express();
 
 const {
@@ -11,44 +12,52 @@ const {
   SecretKey,
 } = require('./config');
 
-const pic = require('./pic');
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use('/example', express.static('example'));
+app.use('/jquery', express.static('node_modules/jquery'));
 
-app.get('/merge', function (req, res) {
+app.post('/merge', function (req, res) {
   const sign = getSign();
-
-  const body = {
-    "app_id": AppID,
-    // "project": "youtu",
-    // "sid": "cf_lover_fanli",
-    // "raw_base64": pic, // base64 编码后的输入图像
-    "img_data": pic, // base64 编码后的输入图像
-    "rsp_img_type": "url",
-    "opdata": [{ //注意opdata是一个数组
-      "cmd": "doFaceMerge",
-      "params": {
-        "model_id": "cf_lover_fanli" // 通用模板id
-      }
-    }]
-  };
-
-  request({
-    method: 'POST',
-    uri: 'http://api.youtu.qq.com/cgi-bin/pitu_open_access_for_youtu.fcg',
-    json: true,
-    resolveWithFullResponse: true,
-    headers: {
-      'Host': 'api.youtu.qq.com',
-      'Content-Type': 'text/json',
-      'Authorization': sign,
-      'Content-Length': byteLength(JSON.stringify(body))
-    },
-    body,
-  }).then(data => {
-    res.send(data);
-  }, (data) => {
-    res.send(data);
+  let data = "";
+  req.on("data", function (chunk) {
+    data += chunk;
   })
+  req.on("end", function () {
+    // console.log('data', img_data);
+    data = JSON.parse(data);
 
+    const img_data = data.img_data;
+    const body = {
+      "app_id": AppID,
+      "img_data": img_data, // base64 编码后的输入图像
+      "rsp_img_type": "url",
+      "opdata": [{ //注意opdata是一个数组
+        "cmd": "doFaceMerge",
+        "params": {
+          "model_id": data.model_id // 通用模板id
+        }
+      }]
+    };
+
+    request({
+      method: 'POST',
+      uri: 'http://api.youtu.qq.com/cgi-bin/pitu_open_access_for_youtu.fcg',
+      json: true,
+      resolveWithFullResponse: true,
+      headers: {
+        'Host': 'api.youtu.qq.com',
+        'Content-Type': 'text/json',
+        'Authorization': sign,
+        'Content-Length': byteLength(JSON.stringify(body))
+      },
+      body,
+    }).then(data => {
+      res.send(data.body);
+    }, (data) => {
+      res.send(data.body);
+    })
+  })
 });
 
 var server = app.listen(3000, function () {

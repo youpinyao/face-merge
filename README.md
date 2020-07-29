df9ba08f260cb939fe02dbe08bd7fd5c1109e028
youpinyao <497400448@qq.com>
2020-07-29 09:53:03 +0800

    refactor(移植): 移植

---
+++ README.md
@@ -0,0 +1,3 @@
+# 已移植
+
+[https://code.aliyun.com/497400448/face-merge](https://code.aliyun.com/497400448/face-merge)
--- src/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  qq: '497400448',
-  AppID: '10135814',
-  SecretID: 'AKIDyTAHXaaJm5970zq2V7czT5nZD0raDWm7',
-  SecretKey: 'M9ohhpneMC7bVyscBbwOix41154tWyNB',
-}
